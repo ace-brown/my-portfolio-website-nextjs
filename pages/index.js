03e5dd048c4bc933035bbc788b07d4206fba96a9
ace-brown <kazem_abdi@yahoo.com>
2023-05-28 18:16:44 +0200

    refactor: projects page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 import React from "react";
 
-import { getFeaturedProject } from "../dummy-project";
-import ProjectList from "../components/projects/project-list";
-
 export default function HomePage() {
-  const featuredProjects = getFeaturedProject();
-
-  return (
-    <ul>
-      <ProjectList items={featuredProjects} />
-    </ul>
-  );
+  return <div>home</div>;
 }

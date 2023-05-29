7764daccb5c490335bd517b210a823b9cfb76c3e
ace-brown <kazem_abdi@yahoo.com>
2023-05-29 22:28:58 +0200

    add: nav bar syles

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,57 +1,60 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 import classes from "./main-header.module.css";
 import styles from "../../styles/shame.module.css";
+import { SidebarData } from "./sidebar-data";
+import { IconContext } from "react-icons";
 
-import iconClose from "../../public/images/iconClose";
-import iconHamburger from "../../public/images/iconHamburger";
+export default function MainHeader(props) {
+  const [toggle, setToggle] = useState(false);
 
-export default function MainHeader() {
+  const sidebarHandler = () => {
+    setToggle(!toggle);
+  };
+
+  // ${styles.container}
   return (
-    <header className={`${styles.container} ${classes.header}`}>
-      <Link href="/" className={classes.logo}>
-        Kazem Abdi
-      </Link>
-      <button
-        className={classes["mobile-nav-toggle"]}
-        area-aria-controls="primary-nav"
-      >
-        <img
-          className={classes["icon-close"]}
-          src={iconClose}
-          alt=""
-          aria-hidden="true"
-        />
-        <img
-          className={classes["icon-hamburger"]}
-          src={iconHamburger}
-          alt=""
-          aria-hidden="true"
-        />
-        <span className={classes["visually-hidden"]}>menue</span>
-      </button>
-      <nav className={classes.navigation}>
-        <ul
-          aria-label="Primary"
-          role="list"
-          className={classes["nav-list"]}
-          id="primary-nav"
-        >
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link href="/">Contact</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <header className={` ${classes.header}`}>
+          <div className={classes.navbar}>
+            <Link href="/" className={classes.logo}>
+              Kazem Abdi
+            </Link>
+            <Link href="/" className={classes["menu-bars"]}>
+              <FaBars onClick={sidebarHandler} />
+            </Link>
+          </div>
+          <nav
+            className={
+              toggle
+                ? `${classes["nav-menu"]} ${classes.active}`
+                : classes["nav-menu"]
+            }
+          >
+            <ul className={classes["nav-menu-items"]} onClick={sidebarHandler}>
+              <li>
+                <Link href="/">
+                  <AiOutlineClose className={classes["svg-icon"]} />
+                </Link>
+              </li>
+              {SidebarData.map((data, index) => {
+                return (
+                  <li key={index} className={data.cName}>
+                    <Link href={data.path}>
+                      {data.icon}
+                      <span>{data.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </header>
+      </IconContext.Provider>
+    </>
   );
 }

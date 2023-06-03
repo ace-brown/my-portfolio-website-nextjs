545e9071d3d1ae3b43b968107add31aeb67a6a39
ace-brown <kazem_abdi@yahoo.com>
2023-06-03 14:01:17 +0200

    add: vide backgroud

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -7,8 +7,6 @@ import { SidebarData } from "./sidebar-data";
 import { IconContext } from "react-icons";
 
 import classes from "./main-header.module.css";
-import styles from "../../styles/shame.module.css";
-import logo from "../../public/images/Logo.png";
 
 export default function MainHeader(props) {
   const [toggle, setToggle] = useState(false);
@@ -19,44 +17,46 @@ export default function MainHeader(props) {
 
   // ${styles.container}
   return (
-    <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <header className={` ${classes.header}`}>
-          <div className={classes.navbar}>
-            <Link href="/" className={classes.logo}>
-              <img src="/images/Logo.png" alt="Logo" width={150} height={100} />
-            </Link>
-            <Link href="/" className={classes["menu-bars"]}>
-              <FaBars onClick={sidebarHandler} />
-            </Link>
-          </div>
-          <nav
-            className={
-              toggle
-                ? `${classes["nav-menu"]} ${classes.active}`
-                : classes["nav-menu"]
-            }
-          >
-            <ul className={classes["nav-menu-items"]} onClick={sidebarHandler}>
-              <li className={classes["nav-text"]}>
-                <Link href="/">
-                  <AiOutlineClose className={classes["svg-icon"]} />
-                </Link>
-              </li>
-              {SidebarData.map((data, index) => {
-                return (
-                  <li key={index} className={data.cName}>
-                    <Link href={data.path}>
-                      {data.icon}
-                      <span>{data.title}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </header>
-      </IconContext.Provider>
-    </>
+    <div className={classes.hero}>
+      {/* Video by ROMAN ODINTSOV: https://www.pexels.com/video/drone-shot-of-the-coast-6662984/ */}
+      <video>
+        <source src="/shore.mp4" type="video/mp4" />
+      </video>
+      <header className={` ${classes.header}`}>
+        <div className={classes.navbar}>
+          <Link href="/" className={classes.logo}>
+            <img src="/images/Logo.png" alt="Logo" width={130} height={90} />
+          </Link>
+          <Link href="#" className={classes["menu-bars"]}>
+            <FaBars onClick={sidebarHandler} />
+          </Link>
+        </div>
+        <nav
+          className={
+            toggle
+              ? `${classes["nav-menu"]} ${classes.active}`
+              : classes["nav-menu"]
+          }
+        >
+          <ul className={classes["nav-menu-items"]} onClick={sidebarHandler}>
+            <li className={classes["nav-text"]}>
+              <Link href="#">
+                <AiOutlineClose className={classes["svg-icon"]} />
+              </Link>
+            </li>
+            {SidebarData.map((data, index) => {
+              return (
+                <li key={index} className={data.cName}>
+                  <Link href={data.path}>
+                    {data.icon}
+                    <span>{data.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }

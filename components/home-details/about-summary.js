e9888a0351f48a0aaecce41305d2fc3ac6804cb2
ace-brown <kazem_abdi@yahoo.com>
2023-06-18 20:13:51 +0200

    refactor: summary

--- components/home-details/about-summary.js
+++ components/home-details/about-summary.js
@@ -19,8 +19,9 @@ export default function AboutSummary() {
           Alongside my proficiency in web development, I am deeply passionate
           about blockchain technology and its potential for driving innovation.
           I am also fond of nature; that's why you see pictures of the sea and
-          jungle. On my portfolio website, you can explore my projects and find
-          opportunities for collaboration to create impactful digital solutions.
+          jungle. On my portfolio website, you can browse through my projects
+          and get a glimpse of my skills and expertise. I'm always eager to
+          connect with fellow professionals and discuss exciting opportunities.
           Let's <Link href="./contact">connect</Link>
           and transform our ideas into reality!
         </p>

346b47f35f7350a246f350c2e1a2692a4f48fc5b
ace-brown <kazem_abdi@yahoo.com>
2023-06-19 21:50:41 +0200

    add all

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "/<repository-name>/" : "",
-  // Replace <repository-name> with the name of your GitHub repository
+    process.env.NODE_ENV === "production"
+      ? "/my-portfolio-website-nextjs/"
+      : "",
 };
